7f1ed1cc4a60309948313faaea67676125339384
Geo Pertea <geo.pertea@gmail.com>
2021-02-16 01:05:33 -0500

    refactored without arrow function!

--- axiostst/src/App.js
+++ axiostst/src/App.js
@@ -1,19 +1,54 @@
 import react, {useState} from 'react';
-import { search } from './utils';
-
+//import { search } from './utils';
+import axios from "axios";
 import Movies from "./Movies";
+
+const resources = {};
+let cancel;
+
+async function getAxiosData(query) {
+    if (cancel) {
+      // Cancel the previous request before making a new request
+      cancel.cancel();
+    }
+    // Create a new CancelToken
+    cancel = axios.CancelToken.source();
+    try {
+      if (resources[query]) {
+        // Return result if it exists
+        return resources[query];
+      }
+      const res = await axios(query, { cancelToken: cancel.token });
+
+      const result = res.data.results;
+      // Store response
+      resources[query] = result;
+
+      return result;
+    } catch (error) {
+      if (axios.isCancel(error)) {
+        // Handle if request was cancelled
+        console.log("Request canceled", error.message);
+      } else {
+        // Handle usual errors
+        console.log("Something went wrong: ", error.message);
+      }
+    }
+};
+
 function App() {
   const [movies, setMovies] = useState(null);
-  const [loading, setLoading] = useState(false);
+  //const [loading, setLoading] = useState(false);
   const [value, setValue] = useState('');
 
   const searchMovies = async val => {
-    setLoading(true);
-    const results = await search(
+    //setLoading(true);
+    // const results = await search(
+    const results = await getAxiosData(
       `https://api.themoviedb.org/3/search/movie?query=${val}&api_key=dbc0a6d62448554c27b6167ef7dabb1b`
     );
     setMovies(results); 
-    setLoading(false);
+    //setLoading(false);
   };
 
   const onChangeHandler = async (e) => {
